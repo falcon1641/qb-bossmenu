97a182da76ca2ca9e68edb3c06427ee164361344
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-14 03:22:58 -0500

    add jobs and updated locations

--- config.lua
+++ config.lua
@@ -1,10 +1,13 @@
 Config = {}
 
 Config.Jobs = {
-    ['police'] = vector3(448.4, -973.2, 30.6),
+    ['police'] =  vector3(463.23, -984.87, 30.7),
     ['ambulance'] = vector3(310.1, -599.43, 43.29),
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-339.53, -157.34, 44.59),
+    ['burger'] = vector3(-1203.13, -895.26, 14.0),
+    ['judge'] = vector3(258.39, -407.61, 47.46),
+    ['governor'] = vector3(224.97, -416.42, 47.98)
 }
